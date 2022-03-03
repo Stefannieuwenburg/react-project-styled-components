51f56cfc8c2a9725aa9f3d42e08fc80d5059f8ca
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-03-03 16:09:38 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { ButtonProps } from "./Components/ButtonProps.style";
+import { AppContainer } from "./Components/Container.style";
+import { Button,GreenButton} from "./Components/Button.style"
+
+
+export default function App() {
+  return (
+      <AppContainer>
+          <h1>Style your components in react whith styled-components</h1>
+          <br />
+          <ButtonProps backgroundColor="red">Click this button</ButtonProps>
+      <ButtonProps backgroundColor="violet">Click this button</ButtonProps>
+      <Button>click me</Button>
+      <GreenButton>click me</GreenButton>
+      </AppContainer>
+  );
+}
+
+
---
+++ src/Components/Button.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+width:200px;
+height:50px;
+background-color:blue;
+
+`;
+export const GreenButton = styled.button`
+    width: 200px;
+    height: 50px;
+    background-color: Green;
+`;
